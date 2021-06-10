49c635c85f269e33e080e86859c55e23cd108d75
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-06-10 00:42:35 -0300

    removed unecessary files

--- src/graficos/Spritesheet.java
+++ src/graficos/Spritesheet.java
@@ -5,6 +5,8 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+import java.io.File;
+
 public class Spritesheet {
 	
 	//Objeto com o spritesheet
@@ -12,7 +14,8 @@ public class Spritesheet {
 	
 	//Capturar o spritesheet no diretorio em "path"
 	public Spritesheet(String path) throws IOException {
-		spritesheet = ImageIO.read(getClass().getResource(path));
+		File f = new File(System.getProperty("user.dir") + "/res/" + path);
+		spritesheet = ImageIO.read(f);
 	}
 	
 	//Pegar um sprite dentro do spritesheet
